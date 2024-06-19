af1f926da6a974910eb4e75f1f8a56f641313dcd
DarkClaws1 <thisboring@hotmail.com>
2024-06-19 15:54:31 -0400

    Figured it out

--- DC_Picture/Code.lua
+++ DC_Picture/Code.lua
@@ -1,10 +1,14 @@
 function Initialize()
-	path = SKIN:GetVariable('path')
-	print(path)
-	local lfs = require("lfs")
-	for filename in lfs.dir(path) do
-			if filename ~= "." and filename ~= ".." then
-					print("\t" .. filename)
-			end
-	end
+	path = SKIN:GetVariable('path')	
+	--local lfs = require("lfs")
+	--for filename in lfs.dir(path) do
+	--		if filename ~= "." and filename ~= ".." then
+	--				print("\t" .. filename)
+	--		end
+	--end
+end
+
+function Calculate()
+	mCount = SKIN:GetMeasure('MeasureCount')
+	print(tonumber(mCount:GetValue()))
 end
